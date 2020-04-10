df5c01c21614f3e41dbf82fe7b1d1ecf1ac819b3
nour.khaled <nour.khaled@arup.com>
2020-04-10 23:37:22 +0100

    Add class to preprocess all the tweets text before applying any sentiment classification

--- message-polarity-classifier.py
+++ message-polarity-classifier.py
@@ -1,8 +1,36 @@
 import argparse
 import pandas as pd
 import os
+import re
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from string import punctuation
 
 
+# Pre-processing the tweets before applying any sentiment classification
+
+class PreProcessTweets:
+    def __init__(self):
+        self._stopwords = set(stopwords.words('english') + list(punctuation) + ['AT_USER','URL'])
+
+    def process_tweets(self, list_of_tweets):
+        processedTweets=[]
+        for tweet in list_of_tweets:
+            processedTweets.append((self._processTweet(tweet["Tweet"]),tweet["Sentiment"]))
+        return processedTweets
+
+    def _processTweet(self, tweet):
+        # change all text to lowercase
+        tweet = tweet.lower()
+        # remove all the meaningless repeated characters from the tweets to unify the words
+        tweet = word_tokenize(tweet)
+        # remove all links and URLs from the tweets
+        tweet = re.sub('((www\.[^\s]+)|(https?://[^\s]+))', 'URL', tweet)
+        # remove all the hashtag symbols form the tweets
+        tweet = re.sub(r'#([^\s]+)', r'\1', tweet)
+        # remove all the usernames if any
+        tweet = re.sub('@[^\s]+', 'AT_USER', tweet)
+        return [word for word in tweet if word not in self._stopwords]
 
 
 if __name__ == '__main__':
