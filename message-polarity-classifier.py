5736e25139e75329a9b08912b2ca70efc2d944e9
nour.khaled <nour.khaled@arup.com>
2020-04-14 09:32:05 +0100

    Add function to load twitter data features and labels and split into training, validation and test sets

--- message-polarity-classifier.py
+++ message-polarity-classifier.py
@@ -5,6 +5,27 @@ import re
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from string import punctuation
+from nltk import FreqDist
+from nltk import classify
+from nltk import NaiveBayesClassifier
+import random
+import pickle
+from nltk.classify.scikitlearn import SklearnClassifier
+from sklearn.naive_bayes import MultinomialNB,BernoulliNB
+from sklearn.linear_model import LogisticRegression,SGDClassifier
+from sklearn.svm import SVC, LinearSVC, NuSVC
+
+
+def get_features_and_labels(tweets_directory):
+    tweets = pd.read_csv(tweets_directory, sep='\t')
+    tweets.columns = ['TweetID', 'Sentiment', 'Tweet']
+    tweets_training = tweets[:4199]
+    tweets_validation = tweets[4200:5099]
+    tweets_test = tweets[5100:6000]
+    dict_tweets_training = tweets_training.to_dict('records')
+    dict_tweets_validation = tweets_validation.to_dict('records')
+    dict_tweets_test = tweets_test.to_dict('records')
+    return dict_tweets_training, dict_tweets_validation, dict_tweets_test
 
 
 # Pre-processing the tweets before applying any sentiment classification
@@ -24,12 +45,6 @@ class PreProcessTweets:
         tweet = tweet.lower()
         # remove all the meaningless repeated characters from the tweets to unify the words
         tweet = word_tokenize(tweet)
-        # remove all links and URLs from the tweets
-        tweet = re.sub('((www\.[^\s]+)|(https?://[^\s]+))', 'URL', tweet)
-        # remove all the hashtag symbols form the tweets
-        tweet = re.sub(r'#([^\s]+)', r'\1', tweet)
-        # remove all the usernames if any
-        tweet = re.sub('@[^\s]+', 'AT_USER', tweet)
         return [word for word in tweet if word not in self._stopwords]
 
 
@@ -43,8 +58,7 @@ if __name__ == '__main__':
 
     args = vars(arg_parser.parse_args())
     tweets_directory = args['tweets_dir']
-    print("Building sentiment classification model from Twitter tweets (text messages) in {},"
-          "Index of sentiment field in the txt file is".format(tweets_directory))
+    print("Building sentiment classification model from Twitter tweets (text messages) in {}.".format(tweets_directory))
 
     cwd = os.getcwd()
     csv_file_path = cwd + '/tweets_and_sentiment.csv'
@@ -54,8 +68,44 @@ if __name__ == '__main__':
     tweetProcessor = PreProcessTweets()
     preprocessedTrainingSet = tweetProcessor.process_tweets(dict_tweets)
 
+    MNB_classifier = SklearnClassifier(MultinomialNB())
+    MNB_classifier.train(training_features)
+    print("MultinomialNB accuracy percent:", classify.accuracy(MNB_classifier, validation_features))
 
+    BNB_classifier = SklearnClassifier(BernoulliNB())
+    BNB_classifier.train(training_features)
+    print("BernoulliNB accuracy percent:", classify.accuracy(BNB_classifier, validation_features))
 
+    print("Original Naive Bayes Algo accuracy percent:", (classify.accuracy(classifier, validation_features))*100)
+    classifier.show_most_informative_features(15)
+
+    MNB_classifier = SklearnClassifier(MultinomialNB())
+    MNB_classifier.train(training_features)
+    print("MNB_classifier accuracy percent:", (classify.accuracy(MNB_classifier, validation_features))*100)
+
+    BernoulliNB_classifier = SklearnClassifier(BernoulliNB())
+    BernoulliNB_classifier.train(training_features)
+    print("BernoulliNB_classifier accuracy percent:", (classify.accuracy(BernoulliNB_classifier, validation_features))*100)
+
+    LogisticRegression_classifier = SklearnClassifier(LogisticRegression())
+    LogisticRegression_classifier.train(training_features)
+    print("LogisticRegression_classifier accuracy percent:", (classify.accuracy(LogisticRegression_classifier, validation_features))*100)
+
+    SGDClassifier_classifier = SklearnClassifier(SGDClassifier())
+    SGDClassifier_classifier.train(training_features)
+    print("SGDClassifier_classifier accuracy percent:", (classify.accuracy(SGDClassifier_classifier, validation_features))*100)
+
+    SVC_classifier = SklearnClassifier(SVC())
+    SVC_classifier.train(training_features)
+    print("SVC_classifier accuracy percent:", (classify.accuracy(SVC_classifier, validation_features))*100)
+
+    LinearSVC_classifier = SklearnClassifier(LinearSVC())
+    LinearSVC_classifier.train(training_features)
+    print("LinearSVC_classifier accuracy percent:", (classify.accuracy(LinearSVC_classifier, validation_features))*100)
+
+    NuSVC_classifier = SklearnClassifier(NuSVC())
+    NuSVC_classifier.train(training_features)
+    print("NuSVC_classifier accuracy percent:", (classify.accuracy(NuSVC_classifier, validation_features))*100)
 
 
 
