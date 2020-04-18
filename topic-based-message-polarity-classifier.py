bb25a21d2a1bd17ce88d78194fb155ba2c41bd80
nour.khaled <nour.khaled@arup.com>
2020-04-18 17:58:19 +0100

    Adding the same functions in message-polarity-classifier.py script to this script

---
+++ topic-based-message-polarity-classifier.py
@@ -0,0 +1,99 @@
+import argparse
+import pandas as pd
+import os
+import re
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from string import punctuation
+from nltk import FreqDist
+from nltk import classify
+from nltk import NaiveBayesClassifier
+import random
+
+
+def get_features_and_labels(tweets_directory):
+    tweets = pd.read_csv(tweets_directory, sep='\t')
+    tweets.columns = ['TweetID', 'Topic', 'Sentiment', 'Tweet']
+    tweets = tweets[tweets['Tweet'] != 'Not Available']
+    tweets_training = tweets[:2109]
+    tweets_validation = tweets[2110:2561]
+    tweets_test = tweets[2562:3014]
+    dict_tweets_training = tweets_training.to_dict('records')
+    dict_tweets_validation = tweets_validation.to_dict('records')
+    dict_tweets_test = tweets_test.to_dict('records')
+    return dict_tweets_training, dict_tweets_validation, dict_tweets_test
+
+
+# Pre-processing the tweets before applying any sentiment classification
+
+class PreProcessTweets:
+    def __init__(self):
+        self._stopwords = set(stopwords.words('english') + list(punctuation) + ['AT_USER','URL'])
+
+    def process_tweets(self, list_of_tweets):
+        processedTweets=[]
+        for tweet in list_of_tweets:
+            processedTweets.append((self._processTweet(tweet["Tweet"]),tweet["Topic"], tweet["Sentiment"]))
+            random.shuffle(processedTweets)
+        return processedTweets
+
+    def _processTweet(self, tweet):
+        # change all text to lowercase
+        tweet = tweet.lower()
+        # remove all links and URLs from the tweets
+        tweet = re.sub('((www\.[^\s]+)|(https?://[^\s]+))', 'URL', tweet)
+        # remove all the usernames if any
+        tweet = re.sub('@[^\s]+', 'AT_USER', tweet)
+        # remove all the hashtag symbols form the tweets
+        tweet = re.sub(r'#([^\s]+)', r'\1', tweet)
+        # remove all the meaningless repeated characters from the tweets to unify the words
+        tweet = word_tokenize(tweet)
+        return [word for word in tweet if word not in self._stopwords]
+
+
+# Function to build the vocabulary of all the words that exist in the tweets
+def create_wordbook(preprocessed_training_data):
+    all_words = []
+
+    for (words, topic, sentiment) in preprocessed_training_data:
+        all_words.extend(words)
+
+    word_list = FreqDist(all_words)
+    word_features = word_list.keys()
+    return word_features
+
+
+def extract_tweet_features(tweet):
+    tweet_words = set(tweet)
+    features = {}
+    for word in word_features:
+        features['contains(%s)' % word] = (word in tweet_words)
+    return features
+
+def keyfunc(x):
+    return x['Topic']
+
+
+if __name__ == '__main__':
+
+    arg_parser = argparse.ArgumentParser(
+        description='ML model trained on Twitter tweets to classify sentiment from input tweets or messages.')
+    arg_parser.add_argument('-i', '--tweets-dir',
+                            help='the path to the directory where the tweets and labels are',
+                            required=True)
+
+    args = vars(arg_parser.parse_args())
+    tweets_directory = args['tweets_dir']
+    print("Building sentiment classification model from Twitter tweets (text messages) in {}.".format(tweets_directory))
+
+    dict_tweets_training, dict_tweets_validation, dict_tweets_test = get_features_and_labels(tweets_directory)
+    tweetProcessor = PreProcessTweets()
+    preprocessed_training_data = tweetProcessor.process_tweets(dict_tweets_training)
+    preprocessed_validation_data = tweetProcessor.process_tweets(dict_tweets_validation)
+    preprocessed_test_data = tweetProcessor.process_tweets(dict_tweets_test)
+
+    word_features = create_wordbook(preprocessed_training_data)
+    training_features = classify.apply_features(extract_tweet_features, preprocessed_training_data)
+    validation_features = classify.apply_features(extract_tweet_features, preprocessed_validation_data)
+    test_features = classify.apply_features(extract_tweet_features, preprocessed_test_data)
+
